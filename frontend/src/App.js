a3053a277bb18af099c44f61abdbd2826a9cd9d2
carlo <carlo@MacBook-Pro.station>
2024-03-17 19:22:35 +0100

    first dev commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Stack } from '@mui/material';
+
+import './App.css';
+import BodyApp from './components/BodyApp'
+import HeaderApp from './components/HeaderApp'
+// import FooterApp from './components/FooterApp'
+
+function App() {
+  return (
+    <Stack className='App' height="100vh" spacing={2}>
+      <HeaderApp />
+      <BodyApp />
+      {/* <FooterApp /> */}
+    </Stack>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/BodyApp.js
@@ -0,0 +1,11 @@
+import { Stack } from '@mui/material';
+
+const BodyApp = () => {
+  return (
+    <Stack className='BodyApp' >
+      ue
+    </Stack>
+  )
+}
+
+export default BodyApp
